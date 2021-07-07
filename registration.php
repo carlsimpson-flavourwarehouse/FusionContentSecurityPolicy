1251c48b22d0ceed7c34b5a48fd7f3a9e97b3302
carl simpson <carl.simpson@flavourwarehouse.co.uk>
2021-07-07 18:08:58 +0100

    add files to repo

---
+++ registration.php
@@ -0,0 +1,4 @@
+<?php \Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE, 'Flavour_FusionContentSecurityPolicy',
+    __DIR__
+);
